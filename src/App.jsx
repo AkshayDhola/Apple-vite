d984d6363c43662142b49a0c844f6dad37f53e98
Akshay <171118842+AkshayDhola@users.noreply.github.com>
2025-05-24 14:59:06 +0530

    new animation

--- src/App.jsx
+++ src/App.jsx
@@ -1,31 +1,32 @@
-import MacContainer from "./MacContainer";
+import MacContainer from "./Components/MacContainer";
 import { Canvas } from "@react-three/fiber";
-import { Environment, OrbitControls, ScrollControls } from "@react-three/drei";
+import { Environment, ScrollControls } from "@react-three/drei";
+import anchors from "./Objects/anchors";
+
 function App() {
   return (
     <div className="w-full h-screen">
-      <div className="absolute flex gap-10 py-4 top-0 left-1/2 -translate-x-1/2">
-        {["iphone", "ipad","services","ios","mac","products", "ipad","services","ios","mac","products"].map(e => {
-          return <a href="" className="text-white font-[400] text-sm capitalize">
-            {e}
-          </a>
-        })}
+      <div className="absolute w-4 h-full bg-black z-[100] right-0 top-0"></div>
+      <div className="absolute w-full px-32 pt-4 flex items-center justify-between z-[100]">
+          {Object.entries(anchors).map(([text, href])=> (
+            <a href={href} key={text} className="text-white text-sm hover:cursor-pointer">
+              {text}
+            </a>
+          ))}
       </div>
-      <div className="absolute flex flex-col items-center text-white top-36 left-1/2 -translate-x-1/2 -translate-y-1/2">
-        <h3 className="mask text-7xl tracking-tight font-[700]">Macbook Pro.</h3>
-        <h5>oh so pro!</h5>
-        <p className="w-3/4 text-center">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ab tenetur exercitationem sunt?</p>
+      <div className="absolute flex flex-col items-center w-4/5 text-white top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2">
+        <h3 className="mask text-[9vw] tracking-tight font-[700]">Macbook Pro.</h3>        
       </div>
-    <Canvas camera={{ fov: 12, position: [0, -10, 220] }}>
-      <Environment
-        files={[
-          "https://dl.polyhaven.org/file/ph-assets/HDRIs/exr/4k/brown_photostudio_02_4k.exr",
-        ]}
-      />
-      <ScrollControls pages={3}>
-        <MacContainer />
-      </ScrollControls>
-    </Canvas>
+      <Canvas camera={{ fov: 12, position: [-60, 20, 220] }}>
+        <Environment
+          files={[
+            "https://dl.polyhaven.org/file/ph-assets/HDRIs/exr/4k/brown_photostudio_02_4k.exr",
+          ]}
+        />
+        <ScrollControls pages={3}>
+          <MacContainer />
+        </ScrollControls>
+      </Canvas>
     </div>
   );
 }
---
+++ src/Objects/anchors.js
@@ -0,0 +1,15 @@
+const anchors = {
+  Store: "https://www.apple.com/us/shop/goto/store",
+  Mac: "https://www.apple.com/mac/",
+  iPad: "https://www.apple.com/ipad/",
+  iPhone: "https://www.apple.com/iphone/",
+  Watch: "https://www.apple.com/watch/",
+  Vision: "https://www.apple.com/apple-vision-pro/",
+  AirPods: "https://www.apple.com/airpods/",
+  TvHome: "https://www.apple.com/tv-home/",
+  Entertainment: "https://www.apple.com/entertainment/",
+  Accessories: "https://www.apple.com/us/shop/goto/buy_accessories",
+  Support: "https://support.apple.com/?cid=gn-ols-home-hp-tab",
+};
+
+export default anchors;
